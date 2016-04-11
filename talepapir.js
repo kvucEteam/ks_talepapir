75d47476fa6d252bc9bba4a9ce0e3a5b17c9b946
Anders Tornøe <ato@kvuc.dk>
2016-04-11 13:43:23 +0200

    ux med Tina

--- talepapir.js
+++ talepapir.js
@@ -3,82 +3,98 @@ var txt_selection;
 var json_streng;
 var pageX;
 var pageY;
+var score = 0;
+var step = 0;
 
 $(document).ready(function() {
 
     // alert(jsonData.tekst);
 
-    enable_audio();
+    // enable_audio();
     generateHTML();
 
+    $(".btn_transfer").click(transfer_text);
     $(".btn_tjek").click(tjeksvar);
 
     setInterval(function() {
-        txt_selection = window.getSelection().toString();
-        if (txt_selection.length < 65) {
-            $(".txtudsnit").html(txt_selection);
-        } else {
-            $(".txtudsnit").html("MAX 65 tegn!");
-        }
-        //console.log(txt_selection)
+        update_selection();
     }, 100);
 
-    $(document).bind('mousemove', function(e) {
-        pageX = e.pageX;
-        pageY = e.pageY;
-    });
-
-    $(".txtudsnit").click(function() {
-        if (txt_selection.length > 0) {
-            $(".udklips_container").prepend("<div class='udklips_ord btn btn-info '>" + txt_selection + "</div>");
-        }
-        $(".udklips_ord").eq(0).fadeOut(0);
-        $(".udklips_ord").eq(0).fadeIn(200);
-
-        $(".udklips_ord").draggable({
-            "revert": "invalid"
-        });
-        //$(".draggable_text").css("left", pageX).css("top", pageY + 20);
-    });
 
     $(".dropzone_skrald").droppable({
         drop: function(event, ui) {
             ui.draggable.fadeToggle("slow", function() {
                 ui.draggable.remove();
             });
-
-        }
+        },
+        hoverClass: 'dropping',
     });
 
 });
 
 function generateHTML() {
-    $(".txt_besvarelse").html(jsonData.tekst);
-    $('#explanationWrapper').html(explanation(jsonData.explanation));
+    $(".txt_besvarelse").prepend("<h4>" + jsonData.undersspm + "</h4>" + jsonData.tekst);
+    //$('#explanationWrapper').html(explanation(jsonData.explanation));
     $(".instr_container").html(instruction(jsonData.Instruktion));
+
 }
 
+function update_selection() {
+    txt_selection = window.getSelection().toString();
+    if (txt_selection.length == 0) {
+        $(".udklips_content").html("Marker et ord");
+    } else if (txt_selection.length < 65) {
+        $(".udklips_content").html("'" + txt_selection + "'");
+    } else {
+        $(".udklips_content").html("MAX 65 tegn!");
+    }
+}
+
+function transfer_text() {
+    var exist = false;
+
+    $(".udklips_ord").each(function() {
+        if ($(this).html() == txt_selection) {
+            console.log("Den er der i forvejen!");
+            exist = true;
+        }
+    });
+
+    if (txt_selection.length > 0 && txt_selection.length < 65 && exist === false) {
+        $(".udklips_container").prepend("<div class='udklips_ord btn btn-info '>" + txt_selection + "</div>");
+        $(".udklips_ord").eq(0).fadeOut(0);
+        $(".udklips_ord").eq(0).fadeIn(200);
+
+        $(".udklips_ord").draggable({
+            "revert": "invalid"
+        });
+    }
+}
+
+
 function tjeksvar() {
-    //alert(jsonData.kategorier);
+    score = 0;
     $(".udklips_ord").each(function(index) {
         var tekst = $(this).text().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").toLowerCase();
-        console.log(tekst);
         var tekst_array = tekst.split(" ");
-        console.log(tekst_array);
-
-        //console.log(jsonData.kategorier.indexOf(tekst_array[0]));
 
         for (var i = 0; i < tekst_array.length; i++) {
             if (jsonData.kategorier.indexOf(tekst_array[i]) > -1) {
                 $(this).addClass("btn-success").removeClass("btn-info");
-
-                console.log("korrekt");
+                //alert(tekst_array[i]);
+                var old_html = $(this).html();
+                var new_html = old_html.replace(tekst_array[i], tekst_array[i].toUpperCase());
+                console.log(new_html);
+                $(this).html(new_html);
+                score++;
+                updateScore();
             } else {
-                console.log("ikke korrekt");
+
             };
         }
-
-
-
     });
+}
+
+function updateScore() {
+    $(".QuestionCounter").html(score + " ud af " + jsonData.kategorier.length);
 }
