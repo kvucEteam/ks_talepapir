67d7ef0262cd110e090c19846f4eff6c87e4c7de
Anders Tornøe <ato@kvuc.dk>
2016-04-13 08:34:58 +0200

    Flere css og js rettelser. kamp med focus på udklipsknapperne...

--- talepapir.js
+++ talepapir.js
@@ -5,12 +5,17 @@ var pageX;
 var pageY;
 var score = 0;
 var step = 0;
+var korrekt_Array = [];
 
 $(document).ready(function() {
 
     // alert(jsonData.tekst);
 
     // enable_audio();
+
+    init();
+
+
     generateHTML();
 
     $(".btn_transfer").click(transfer_text);
@@ -36,13 +41,20 @@ function generateHTML() {
     $(".txt_besvarelse").prepend("<h4>" + jsonData.undersspm + "</h4>" + jsonData.tekst);
     //$('#explanationWrapper').html(explanation(jsonData.explanation));
     $(".instr_container").html(instruction(jsonData.Instruktion));
-
 }
+
+function init() {
+    for (var i = 0; i < jsonData.kategorier.length; i++) {
+        korrekt_Array.push(jsonData.kategorier[i]);
+        //alert("hej");
+    }
+    //alert(korrekt_Array);
+};
 
 function update_selection() {
     txt_selection = window.getSelection().toString();
     if (txt_selection.length == 0) {
-        $(".udklips_content").html("Marker et ord");
+        $(".udklips_content").html("Markér ord i teksten");
     } else if (txt_selection.length < 65) {
         $(".udklips_content").html("'" + txt_selection + "'");
     } else {
@@ -57,36 +69,76 @@ function transfer_text() {
         if ($(this).html() == txt_selection) {
             console.log("Den er der i forvejen!");
             exist = true;
+        } else {
+
         }
     });
 
     if (txt_selection.length > 0 && txt_selection.length < 65 && exist === false) {
-        $(".udklips_container").prepend("<div class='udklips_ord btn btn-info '>" + txt_selection + "</div>");
+        $(".udklips_container").append("<div class='udklips_ord btn btn-info '>" + txt_selection + "</div>");
         $(".udklips_ord").eq(0).fadeOut(0);
         $(".udklips_ord").eq(0).fadeIn(200);
 
-        $(".udklips_ord").draggable({
-            "revert": "invalid"
-        });
+        $(".udklips_container").sortable({});
+
+        $(".udklips_ord").eq($(".udklips_ord").length - 1).click(function() {
+            editudklips_ord($(this));
+        })
     }
 }
+
+
+function editudklips_ord(obj) {
+    console.log("clicked");
+    var old_text = obj.text();
+    var indeks = obj.index();
+    //alert(indeks);
+    $(".udklips_ord").off("click");
+
+    $(".udklips_ord").eq(indeks).html("<input type='text' class='edit_field' value='" + old_text + "'>");
+
+    $(".edit_field").focus();
+
+    $(".edit_field").focusout(function() {
+        console.log("FOCUSOUT");
+        var new_text = $(".edit_field").val();
+        $(".edit_field").remove();
+
+        $(".udklips_ord").eq(indeks).html(new_text);
+        $(".udklips_ord").click(function() {
+            editudklips_ord($(this));
+        })
+        tjeksvar();
+    });
+
+
+}
+
 
 
 function tjeksvar() {
     score = 0;
     $(".udklips_ord").each(function(index) {
+
+        console.log("indeks: " + index)
+
         var tekst = $(this).text().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").toLowerCase();
+        var tekst = tekst.replace("<span class='label label-success'>", "");
+        var tekst = tekst.replace("</span>", "");
+
         var tekst_array = tekst.split(" ");
 
         for (var i = 0; i < tekst_array.length; i++) {
-            if (jsonData.kategorier.indexOf(tekst_array[i]) > -1) {
-                $(this).addClass("btn-success").removeClass("btn-info");
+            console.log("looper indeks:" + index + "tekst_array: " + i);
+            if (korrekt_Array.indexOf(tekst_array[i]) > -1) {
+                //$(this).addClass("btn-success").removeClass("btn-info");
                 //alert(tekst_array[i]);
-                var old_html = $(this).html();
-                var new_html = old_html.replace(tekst_array[i], tekst_array[i].toUpperCase());
+                var old_html = tekst;
+                var new_html = old_html.replace(tekst_array[i], "<span class='label label-success'>" + tekst_array[i] + "</span>");
                 console.log(new_html);
                 $(this).html(new_html);
-                score++;
+                //korrekt_Array.splice(index, 1);
+                //score++;
                 updateScore();
             } else {
 
