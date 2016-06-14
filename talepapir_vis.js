eba96ea66ebb760d33cbb2fd9c6aeee71e87b107
Anders Tornøe <ato@kvuc.dk>
2016-06-14 16:58:25 +0200

    autoCommit by terminal

--- talepapir_vis.js
+++ talepapir_vis.js
@@ -3,10 +3,7 @@ var json_streng;
 
 $(document).ready(function() {
 
-   
-      
-      $(".instr_container").html(instruction("Et tale papir hjælper dig til at strukturere din mundtlige fremlæggelse til eksamen. <br/> Her kan du se den samme besvarelse af et eksamensspørgsmål som en lang tekst, som sætninger og som stikord."));
-
+    $(".instr_container").html(instruction("Et tale papir hjælper dig til at strukturere din mundtlige fremlæggelse til eksamen. <br/> Her kan du se den samme besvarelse af et eksamensspørgsmål som en lang tekst, som sætninger og som stikord."));
     $(".btn_prosa").click(vis_prosa);
     $(".btn_sentence").click(vis_sentence);
     $(".btn_stikord").click(vis_stikord);
@@ -46,12 +43,12 @@ function vis_stikord() {
 function tweenMe() {
     $(".textHolder").animate({
         opacity: "0"
-        //left: "-=1000",
-    }, 50, function() {
+            //left: "-=1000",
+    }, 0, function() {
         $(".textHolder").animate({
             opacity: "1"
-            //left: "+=1000",
-        }, 100, function() {
+                //left: "+=1000",
+        }, 400, function() {
             // Animation complete.
         });
     });
